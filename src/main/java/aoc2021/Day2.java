e10ca9b27ad4551f0b4ae4f3df0f7548712d58eb
Tim Sawyer <tim.sawyer01@dixonscarphone.com>
2021-12-01 17:40:09 +0000

    Day 1 done

---
+++ src/main/java/aoc2021/Day2.java
@@ -0,0 +1,17 @@
+package aoc2021;
+
+public class Day2 {
+    String[] input;
+
+    public Day2(String[] testInput) {
+        this.input = testInput;
+    }
+
+    public long execute1() {
+        return 0;
+    }
+
+    public long execute2() {
+        return 0;
+    }
+}
--- src/test/java/aoc2021/Day2Tests.java
+++ src/test/java/aoc2021/Day2Tests.java
@@ -7,37 +7,37 @@ import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Day1Tests {
+public class Day2Tests {
 
     @Test
     void testPart1BasicInputWorks() {
         String[] testInput = new String[] {"199", "200", "208", "210", "200", "207", "240", "269", "260", "263"};
 
-        Day1 day1 = new Day1(testInput);
-        assertEquals(7, day1.execute1());
+        Day2 day2 = new Day2(testInput);
+        assertEquals(7, day2.execute1());
     }
 
     @Test
     void testPart2BasicInputWorks() {
         String[] testInput = new String[] {"199", "200", "208", "210", "200", "207", "240", "269", "260", "263"};
 
-        Day1 day1 = new Day1(testInput);
-        assertEquals(5, day1.execute2());
+        Day2 day2 = new Day2(testInput);
+        assertEquals(5, day2.execute2());
     }
 
     @Test
     void calculateRealAnswerPart1() throws IOException, URISyntaxException {
-        String[] testInput = FileReader.read("Day1.txt");
+        String[] testInput = FileReader.read("day2.txt");
 
-        Day1 day1 = new Day1(testInput);
-        System.out.println(day1.execute1());
+        Day2 day2 = new Day2(testInput);
+        System.out.println(day2.execute1());
     }
 
     @Test
     void calculateRealAnswerPart2() throws IOException, URISyntaxException {
-        String[] testInput = FileReader.read("Day1.txt");
+        String[] testInput = FileReader.read("day2.txt");
 
-        Day1 day1 = new Day1(testInput);
-        System.out.println(day1.execute2());
+        Day2 day2 = new Day2(testInput);
+        System.out.println(day2.execute2());
     }
 }
